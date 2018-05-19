a61f4b79980cf07d8bdfa44be8b9c1be3d02ec7c
Jack Tan <jacktan165@gmail.com>
2018-05-19 18:06:32 +1200

    Fix errors

--- src/api/v1/user/services/userService.ts
+++ src/api/v1/user/services/userService.ts
@@ -5,11 +5,12 @@ import userRepository from "../repositories";
 const router = Router();
 
 (async () => {
+    // environment variables
     const db = await userRepository({ connectionString: "localhost", connectionPort: 27017, dbName: "indieTruckUser", collection: "user", username: "", password: "" });
 
     // Get all users
-    router.get("/", () => {
-
+    router.get("/", ({ }, res: Response) => {
+        res.send("Woo!");
         // db.get({});
     });
 
@@ -20,31 +21,33 @@ const router = Router();
         res.send(user);
     });
 
+    // Add swagger
     // Create user
-    router.put("/", (req: Request, res: Response) => {
-        console.log(req);
+    router.put("/", ({ }, res: Response) => {
         res.send("get user details via object");
+        // 200 and 201
     });
 
     // Delete user
     router.delete("/:id", () => {
-        console.log("delete");
+        // 202
     });
 
     // Add friend
-    router.patch("/:id/friend/:friendId", (req: Request) => {
-        const friendId = req.params.friendId;
-        if (typeof friendId === "number") {
-            console.log(req.params.id + " add " + friendId);
-        }
+    router.post("/:id/friend/:friendId", ({ }) => {
+        // const friendId = req.params.friendId;
+        // if (typeof friendId === "number") {
+        //     console.log(req.params.id + " add " + friendId);
+        // }
     });
 
     // Delete friend
-    router.delete("/:id/friend/:friendId", (req: Request) => {
-        const friendId = req.params.friend;
-        if (typeof friendId === "number") {
-            console.log("delete friend");
-        }
+    router.delete("/:id/friend/:friendId", ({ }) => {
+        // const friendId = req.params.friend;
+        // if (typeof friendId === "number") {
+        //     console.log("delete friend");
+        // }
+        // 202
     });
 })();
 
